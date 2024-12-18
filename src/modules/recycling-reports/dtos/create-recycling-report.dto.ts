f078906020e10367f0b47e53d482453a80fb5d87
Yuri Mutti <muttiyuri@gmail.com>
2024-12-17 23:12:54 -0300

    refactor: unecessary comments

--- src/modules/recycling-reports/dtos/create-recycling-report.dto.ts
+++ src/modules/recycling-reports/dtos/create-recycling-report.dto.ts
@@ -4,61 +4,57 @@ import { z } from 'zod';
 import { ResidueType } from './residue-type.enum';
 
 const MaterialSchema = z.object({
-  materialType: z.nativeEnum(ResidueType), // Valida como enum
+  materialType: z.nativeEnum(ResidueType),
   weightKg: z
-    .number() // Agora aceita o peso como número diretamente
-    .positive('Weight must be a positive number greater than 0'), // Valida peso como número positivo
+    .number()
+    .positive('Weight must be a positive number greater than 0'),
 });
 
-// Define the main schema for the recycling report
 export const CreateRecyclingReportSchema = z
   .object({
-    submittedBy: z.string().min(1, 'Submitter name cannot be empty'), // Required string
+    submittedBy: z.string().min(1, 'Submitter name cannot be empty'),
     reportDate: z
       .string()
       .optional()
       .refine(
-        (val) => !val || !isNaN(new Date(val).getTime()), // Ensure it's a valid date string
+        (val) => !val || !isNaN(new Date(val).getTime()),
         'Report date must be valid',
       )
-      .transform((val) => (val ? new Date(val) : undefined)) // Transform to a Date object
+      .transform((val) => (val ? new Date(val) : undefined))
       .refine(
-        (date) => !date || date <= new Date(), // Validate that the date is not in the future
+        (date) => !date || date <= new Date(),
         'Report date cannot be in the future',
       ),
-    phone: z.string().optional(), // Optional phone number
+    phone: z.string().optional(),
     materials: z
-      .array(MaterialSchema) // Valida como um array de objetos diretamente
-      .nonempty('Materials array cannot be empty'), // Garante que o array não está vazio
+      .array(MaterialSchema)
+      .nonempty('Materials array cannot be empty'),
     walletAddress: z
       .string()
-      .regex(/^0x[a-fA-F0-9]{40}$/, 'Invalid EVM wallet address format') // Validate Ethereum wallet address
-      .or(z.literal('')) // Allow an empty string
+      .regex(/^0x[a-fA-F0-9]{40}$/, 'Invalid EVM wallet address format')
+      .or(z.literal(''))
       .optional(),
     residueEvidence: z
       .string()
-      .url('Invalid URL format') // Validate as a URL
+      .url('Invalid URL format')
       .refine(
-        (url) => url.startsWith('https://'), // Ensure URL starts with HTTPS
+        (url) => url.startsWith('https://'),
         'Evidence URL must start with https',
       )
       .optional(),
     residueEvidenceFile: z
-      .instanceof(Buffer) // Validate file content as a Buffer
+      .instanceof(Buffer)
       .refine((buffer) => buffer.length > 0, 'File content cannot be empty')
       .optional(),
   })
   .refine((data) => data.residueEvidence || data.residueEvidenceFile, {
     message: 'Either residueEvidenceFile or residueEvidence must be provided.',
-    path: ['residueEvidenceFile', 'residueEvidence'], // Reference both paths for error messages
+    path: ['residueEvidenceFile', 'residueEvidence'],
   });
 
-// Define the TypeScript type based on the schema
 export type CreateRecyclingReportDto = z.infer<
   typeof CreateRecyclingReportSchema
 >;
-
-// Define the Swagger-compatible DTO for documentation purposes
 export class CreateRecyclingReportSwaggerDto {
   @ApiProperty()
   submittedBy: string;
