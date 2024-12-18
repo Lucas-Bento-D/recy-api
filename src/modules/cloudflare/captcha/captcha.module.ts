5364f854bc88f2133394bc5fcc22c224083a7a08
Lucas-Bento-D <lucasbentoskl@outlook.com>
2024-12-17 23:05:18 -0300

    Refactor: remove captcha controller

--- src/modules/cloudflare/captcha/captcha.module.ts
+++ src/modules/cloudflare/captcha/captcha.module.ts
@@ -1,28 +1,27 @@
 import { Module } from '@nestjs/common';
-
-import { CaptchaController } from './captcha.controller';
 import { CaptchaService } from './captcha.service';
 import { TurnstileService } from '../turnstile/turnstile.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ITokenResponse } from './types';
+import { TokenResponse } from './types';
 
 @Module({
   imports: [ConfigModule],
-  controllers: [CaptchaController],
   providers: [
     {
       provide: 'TurnstileServiceOptions',
       useFactory: (config: ConfigService) => {
-          return {
-            secretKey: config.get<string>('CAPTCHA_SECRET_KEY'),
-            host: config.get<string>('CAPTCHA_HOST_VERIFICATION'),
-            tokenResponse: (request: ITokenResponse) => {
-              return request.body.token
-            }
-          }
+        return {
+          secretKey: config.get<string>('CAPTCHA_SECRET_KEY'),
+          host: config.get<string>('CAPTCHA_HOST_VERIFICATION'),
+          tokenResponse: (request: TokenResponse) => {
+            return request.body.token;
+          },
+        };
       },
-      inject: [ConfigService]
+      inject: [ConfigService],
     },
-    CaptchaService, TurnstileService]
+    CaptchaService,
+    TurnstileService,
+  ],
 })
 export class CaptchaModule {}
