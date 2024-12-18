64cd9dcc81f6385ebf6ce4fab26daf916a2112ad
Lucas-Bento-D <lucasbentoskl@outlook.com>
2024-12-17 23:00:23 -0300

    Refactor: remove unnecessary abstration

--- src/modules/cloudflare/turnstile/turnstile.guard.ts
+++ src/modules/cloudflare/turnstile/turnstile.guard.ts
@@ -9,11 +9,6 @@ import {
 import { TurnstileOptions } from './interfaces/turnstile-options.interface';
 import { TurnstileService } from './turnstile.service';
 
-export enum Messages {
-  FAIL = 'Failed turnstile verification.',
-  MISSING = 'Missing turnstile verification code.',
-  INVALID = 'Invalid turnstile verification code.',
-}
 
 @Injectable()
 export class TurnstileGuard implements CanActivate {
@@ -27,13 +22,13 @@ export class TurnstileGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const responseToken = this.options.tokenResponse(request);
 
-    if (!responseToken) throw new BadRequestException(Messages.MISSING);
+    if (!responseToken) throw new BadRequestException('Missing turnstile verification code.');
 
     const { success } = await this.turnstileService.validateToken(
       responseToken,
     );
 
-    if (!success) throw new BadRequestException(Messages.INVALID);
+    if (!success) throw new BadRequestException('Invalid turnstile verification code.');
     return success;
   }
 }
